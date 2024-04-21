0cbdeb1100e002b6cc01a6584faec6e596263d71
chiwenling <147138397+chiwenling@users.noreply.github.com>
2024-04-21 23:35:13 +0800

    0421_final

--- week3/task2.py
+++ week3/task2.py
@@ -2,17 +2,18 @@ print("==task2==")
 import urllib.request as req
 import csv
 
-def get_one_page(url):
+def get_one_page(url, pages):
 
     my_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
               'cookie': 'over18=1;'}
+    
     request = req.Request(url, headers=my_headers)
     with req.urlopen(request) as response:
         data = response.read().decode("utf-8")
 
     import bs4
     root = bs4.BeautifulSoup(data,"html.parser")
-
+    
     #note:找標題
     titles = []
     title_word = root.find_all("div",class_="title")
@@ -47,7 +48,6 @@ def get_one_page(url):
                 post.text = response.read().decode("utf-8")
 
             root2 = bs4.BeautifulSoup(post.text, "html.parser")
-
             rootPost = root2.find("div", id="main-content")
             article_info = rootPost.find_all("span", class_="article-meta-value")
 
@@ -55,22 +55,24 @@ def get_one_page(url):
                 time = article_info[3].string  
             
             page_data.append([titles[i],totals[i],time])
-    return page_data
+
+    nextlink = root.find("a", string="‹ 上頁")["href"]
+    return nextlink, page_data
            
 
-#note:要換三頁
+#note:要換三頁，不可以寫死
 #note:要儲存成csv
 
-start = 2081
-num = 3 
-end = start - num 
+url = "https://www.ptt.cc/bbs/Lottery/index.html"
+pages = int(input("抓頁數多少："))
 
-data=[]
-for i in range(start,end,-1):
-    url="https://www.ptt.cc/bbs/Lottery/index"+str(i)+".html"
-    data.extend(get_one_page(url))
+all_page_data = []
+for page_number in range(1, pages + 1):
+    nextlink, page_data = get_one_page(url, pages)
+    all_page_data.extend(page_data)
+    url = "https://www.ptt.cc" + nextlink
 
 csv_file = "article.csv"
 with open(csv_file, "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
-    writer.writerows(data)
+    writer.writerows(all_page_data)
